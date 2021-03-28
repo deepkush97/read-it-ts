93e81d4ca844f56bc050ea22b3c8dee139d6da96
Deepanshu Kushwaha <deepanshu@atliq.com>
2021-03-28 15:10:15 +0530

    Added: Comments for post

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,4 +1,5 @@
 import { Request, Response, Router } from "express";
+import { Comment } from "../entities/Comment";
 import { Post } from "../entities/Post";
 import { Sub } from "../entities/Sub";
 import { auth } from "../middlewares/auth";
@@ -47,7 +48,22 @@ const getPost = async (req: Request, res: Response) => {
   }
 };
 
+const commentOnPost = async (req: Request, res: Response) => {
+  const { identifier, slug } = req.params;
+  const body = req.body.body;
+  try {
+    const post = await Post.findOneOrFail({ identifier, slug });
+    const comment = new Comment({ body, user: res.locals.user, post });
+    await comment.save();
+    return res.json(comment);
+  } catch (error) {
+    console.error(error);
+    return res.status(404).json({ error: "Post not found" });
+  }
+};
+
 export const postRoutes = Router();
 postRoutes.post("/", auth, createPost);
 postRoutes.get("/", getPosts);
 postRoutes.get("/:identifier/:slug", getPost);
+postRoutes.post("/:identifier/:slug/comments", auth, commentOnPost);
